5895c76c79cb6a253c6e078b28928f9c24a9529c
anthonymunene <munene.anthony@gmail.com>
2020-03-24 11:08:25 +0000

    refactor: completed module with challenge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [result, setResult] = useState([]);
+
+  const incrementCount = increment => {
+    setCount(count + increment);
+  };
+  const decrementCount = decrement => {
+    setCount(count - decrement);
+  };
+  const saveResults = () => {
+    const newResult = [...result, count]
+    setResult(newResult)
+  }
+  const resetCount = () => {
+    setCount(0);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="row">
+          <Button increment={1} onClickFunction={incrementCount} />
+          <Button increment={10} onClickFunction={incrementCount} />
+          <Button increment={100} onClickFunction={incrementCount} />
+          <Button increment={1000} onClickFunction={incrementCount} />
+          <span>{count}</span>
+          <button onClick={resetCount}>Reset</button>
+          <button onClick={saveResults}>Save</button>
+        </div>
+        <div className="row">
+          <Button decrement={1} onClickFunction={decrementCount} />
+          <Button decrement={10} onClickFunction={decrementCount} />
+          <Button decrement={100} onClickFunction={decrementCount} />
+          <Button decrement={1000} onClickFunction={decrementCount} />
+        </div>
+      </div>
     </div>
   );
 }
